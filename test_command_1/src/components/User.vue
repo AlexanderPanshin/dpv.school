3a6bf693b1b8f3eda57666a1ed19d2e9cc4b99a8
Aleks2 <elektrokos@mail.ru>
2019-12-07 21:06:21 +0300

    в bots добавлен iscanceled 50% true а 50% false

--- test_command_1/src/components/User.vue
+++ test_command_1/src/components/User.vue
@@ -1,5 +1,16 @@
 <template>
-    <div class="details">
+  <v-container class="grey lighten-5">
+    <v-row no-gutters style="flex-wrap: nowrap;">
+      <v-col
+        cols="2"
+        class="flex-grow-0 flex-shrink-0"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+             <div class="details">
         <div class="container">
             <div class="row">
                 <!--нижк v for начинаем перебирать массив items -->
@@ -12,7 +23,38 @@
                 </div>
             </div>
         </div>
-    </div>  
+    </div>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="1"
+        style="min-width: 100px; max-width: 100%;"
+        class="flex-grow-1 flex-shrink-0"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          I'm 1 column wide and I grow to take all the space
+        </v-card>
+      </v-col>
+      <v-col
+        cols="5"
+        style="min-width: 100px;"
+        class="flex-grow-0 flex-shrink-1"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          I'm 5 column wide and I shrink if there's not enough space
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+  
 </template>
 <script>
 import axios from 'axios';
