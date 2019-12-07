46a98620e7fc34a6aa12bb5c9a2663e302de1e3f
Aleks2 <elektrokos@mail.ru>
2019-12-07 13:51:22 +0300

    Взял карточку чата занова в

--- test_command_1/src/components/CardChat.vue
+++ test_command_1/src/components/CardChat.vue
@@ -1,81 +1,56 @@
 <template>
-    
-      <section class="ma-5 mt-10" >
-          <div v-for="post in posts"
-                            outlined
-                            :key="post">
-        <v-hover >
-                <template v-slot="{ hover }" >
-                        
-                <v-card  
-                        class="mx-auto p-3"
-                        max-width="600"   
-                        :elevation="hover ? 24 : 1" 
-                >
-        
-               <div v-if="proId == post.botsid">
-                    <v-img
-                            class="white--text align-end"
-                            height="250px"
-                            :src="post.imgbots"
-                    >
-                    <!--   <v-card-title>Top 10 Australian beaches</v-card-title>-->
-                    </v-img>
-                </div>
-                
-                    <v-card-subtitle class="pb-0">{{post.botsname}}</v-card-subtitle>
+  <v-card
+    class="mx-auto"
+    max-width="500px"
+  >
+  <div v-for="(post,index) in posts" :key="index">
+       <div v-if="proId == post.botsid">
+    <v-img
+      :src="post.imgbots"
+      height="200px"
+    ></v-img>
 
-                    <v-card-text class=" pb-0">
-                        <div><h2 class="text--primary my-1">Senior Software Engineer, Gameplay</h2></div>
-                        <div class="pb-0">California &#183; C++, 5 years experience  &#183; Professional</div>
-                        <h3 class="text--primary my-3" >About our company</h3>
-                            <p class="text--primary">
-                                Blizzard Entertainment is seeking a talented and passionate Sr. Software Engineer, Gameplay to design and architect 
-                                compelling gameplay systems for an unannounced project. As a Sr. Software Engineer focused on gameplay, you will champion
-                                Blizzard’s core value of “Gameplay First” as you collaborate with a small team of designers, engineers, and artists responsible 
-                                for defining the core elements of this exciting new project. 
-                                The ideal candidate will have experience working on gameplay aspects of core combat in modern 1st/3rd person AAA titles. By blending your mastery of 
-                                programming with a critical eye for design, you will help architect and implement an epic gameplay experience. 
-                                Our incubation teams are fast paced and mission critical. We love engineering, geeking out over new technology, and helping move the industry forward.
-                                 Most importantly,
-                                we learn, grow, and ultimately enjoy success as a team, overcoming new challenges together.
-                            </p>
-                            <h3 class="text--primary">Мы предлагаем дорогому соискателю, пройти небольшой опрос,
-                               на основании которого мы будем рассматривать вашу Кандидатуру
-                            </h3>
-                            <p>**пошли вопросы**</p>
-                    </v-card-text>
+    <v-card-title>
+      {{post.botsname}}
+    </v-card-title>
 
-                    <v-card>
-                      
-                       <v-list-item>
-                            
-                        <v-list-item-content>
-                            <v-list-item-title><p>{{post.descriptionbots}}</p></v-list-item-title>
-                            <v-btn-toggle
-                                    v-model="toggle_exclusive"
-                                    class="mx-auto"
-                            >
-                               
-                            </v-btn-toggle>
-                        </v-list-item-content>
-                    </v-list-item>
-                     
-                     
-                    </v-card>   
-            
-                    <v-btn class="ma-3" 
-                                color="cyan lighten-1"
-                                text
-                        >
-                            send
-                        </v-btn>
-                </v-card>
-                
-                </template>
-            </v-hover>
-            </div>
-        </section>  
+    <v-card-subtitle>
+      {{post.botscompany}}
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn text>Share</v-btn>
+
+      <v-btn
+        color="purple"
+        text
+      >
+        Explore
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+       </div>
+    </div>
+  </v-card>
+  
 </template>
 
 <script>
@@ -90,6 +65,7 @@
                 dialog: false,
                 redactPost: false,
                 message: null,
+                show: true,
                 n: 0,
                 items: [],
                 posts: [],
