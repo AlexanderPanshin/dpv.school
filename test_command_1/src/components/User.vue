22f1da5129295f4f53ac7d68c8adcbf86278e332
Aleks2 <elektrokos@mail.ru>
2019-12-08 09:14:20 +0300

    Пытаюсь сделать карточку User

--- test_command_1/src/components/User.vue
+++ test_command_1/src/components/User.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-container class="grey lighten-5">
+  <v-container class="grey lighten-1">
     <v-row no-gutters style="flex-wrap: nowrap;">
       <v-col
-        cols="2"
+        cols="3"
         class="flex-grow-0 flex-shrink-0"
       >
         <v-card
@@ -10,20 +10,72 @@
           outlined
           tile
         >
-             <div class="details">
-        <div class="container">
-            <div class="row">
-                <!--нижк v for начинаем перебирать массив items -->
-                <div class="col-md-12" v-for="(item,index) in items" :key="index"> 
-                <!-- v if  из массива выводим только переданный id -->
-                <div v-if="proId == item.userid">
-                    <h1>{{item.username}}</h1>
-                    <img :src="item.image" >
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>
+          <v-card
+    :loading="loading"
+    class="mx-auto my-12"
+    max-width="374"
+  >
+    <v-img
+      height="250"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+    ></v-img>
+
+    <v-card-title>Cafe Badilico</v-card-title>
+
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0"
+      >
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+
+        <div class="grey--text ml-4">4.5 (413)</div>
+      </v-row>
+
+      <div class="my-4 subtitle-1 black--text">
+        $ • Italian, Cafe
+      </div>
+
+      <div>Small plates, salads & sandwiches an inteimate setting with 12 indoor seats plus patio seating.</div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+    <v-card-title>Tonight's availability</v-card-title>
+
+    <v-card-text>
+      <v-chip-group
+        v-model="selection"
+        active-class="deep-purple accent-4 white--text"
+        column
+      >
+        <v-chip>5:30PM</v-chip>
+
+        <v-chip>7:30PM</v-chip>
+
+        <v-chip>8:00PM</v-chip>
+
+        <v-chip>9:00PM</v-chip>
+      </v-chip-group>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="deep-purple accent-4"
+        text
+        @click="reserve"
+      >
+        Reserve
+      </v-btn>
+    </v-card-actions>
+  </v-card>
         </v-card>
       </v-col>
       <v-col
---
+++ test_command_1/src/components/tessttts.vue
@@ -0,0 +1,14 @@
+    <div class="details">
+        <div class="container">
+            <div class="row">
+                <!--нижк v for начинаем перебирать массив items -->
+                <div class="col-md-12" v-for="(item,index) in items" :key="index"> 
+                <!-- v if  из массива выводим только переданный id -->
+                <div v-if="proId == item.userid">
+                    <h1>{{item.username}}</h1>
+                    <img :src="item.image" >
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
